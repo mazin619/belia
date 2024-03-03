2505dcd5b1b82ec5db9477108e65e4588ac8f72a
mazin619 <mazinfayezmoo6@gmail.com>
2024-03-03 23:37:43 +0200

    changed title

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScreenUtilInit(
       child: MaterialApp(
-        title: 'Belia Car Services',
+        title: 'Belia Car sServices',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           fontFamily: 'Poppins',
