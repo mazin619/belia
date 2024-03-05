a384e5d6e0755dc52481a8494f26044e238d80e3
Ammar <ammar.salim2018@gmail.com>
2024-03-05 10:42:11 +0200

    change

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Belia Cars Services',
+      title: 'Belia Cars Servicessss',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         fontFamily: 'Poppins',
